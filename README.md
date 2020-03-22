cb722ef452d3e2de3de4d1d92af866554d0c105d
Mariusz Korzekwa <maledorak@gmail.com>
2020-03-22 08:33:45 +0100

    Add project name overriding

--- README.md
+++ README.md
@@ -14,6 +14,19 @@ How to use?
 * Create project template based on `example_project.yml` file
 * Run `todoist_templates path/to/project_template.yml`
 
+Supported properties
+--------
+All supported properties based on this api: https://developer.todoist.com/sync/v8/
+
+* `name` - Project name
+* `color` - Project color [integer] from 30 to 49 - [More explanation](https://developer.todoist.com/sync/v8/#colors)
+* `tasks` - Project tasks
+    * `name` - Name of task
+    * `priority` - Priority of task [integer] from 1 to 4
+    * `tasks` - Subtasks
+* `sections` - Project sections
+    * `tasks` - Tasks in section, properties are the same like in base project `tasks`
+
 Todo
 ----
 * Add tests
--- todoist_templates/cli.py
+++ todoist_templates/cli.py
@@ -8,7 +8,7 @@ from .parsers import get_project
 from .validator import validate_project
 
 
-def _generate(path):
+def _generate(path, project_name=None):
     try:
         api = get_api()
     except ApiError as error:
@@ -27,16 +27,20 @@ def _generate(path):
         print("Error:", error)
         sys.exit(1)
 
+    if project_name:
+        print('Using override project name: "{}" instead of: "{}"'.format(project_name, project['name']))
+        project['name'] = project_name
+
     generate_project(api, project)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Generate Todoist project from template.')
     parser.add_argument('path', nargs='?', type=str, help='Path to template file')
-    # parser.add_argument('path', nargs='?', type=argparse.FileType('r'), default=sys.stdin) // todo improve passing through bash pipe
+    parser.add_argument('--name', type=str, help='Project name override', required=False)
     args = parser.parse_args()
     if not args.path:
         parser.print_help()
         sys.exit(1)
 
-    _generate(args.path)
+    _generate(args.path, project_name=args.name)
